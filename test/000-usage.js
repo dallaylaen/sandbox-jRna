30764b8ee7cf7d520f40fcf7b0eaa53fb0f10010
Konstantin S. Uvarin <khedin@gmail.com>
2019-05-25 13:57:15 +0300

    TST replace HTML with every test

--- test/000-usage.js
+++ test/000-usage.js
@@ -2,26 +2,26 @@
 var should = require('chai').should();
 
 const { JSDOM } = require('jsdom');
-var window = (new JSDOM('<html><body></body></html>')).window;
-
 // jsdom documentation suggests loading scrips by hand via `eval`
 // https://github.com/jsdom/jsdom/wiki/Don't-stuff-jsdom-globals-onto-the-Node-global
 // but screw that for now...
-global.document = window.defaultView;
-global.window = window;
-
+var mock = (new JSDOM('<html><body></body></html>')).window;
+global.document = mock.defaultView;
+global.window = mock;
 var jQuery = require('jquery');
-var root = jQuery("<div/>", { id : "root" });
-root.html('<span id="myid">some text</span>');
 
 var jRna = require( '../js/jRna.js' );
 
-console.log(jRna);
+function html (text) {
+    var root = jQuery("body");
+    root.html(text);
+    return root;
+};
 
 describe( "jRna", () => {
     it( "spawns instances w/o elements", (done) => {
         var rna = new jRna().def("foo", 42);
-        var enzyme = rna.attach(root);
+        var enzyme = rna.attach(html());
 
         enzyme.should.have.property("foo", 42);
 
@@ -30,7 +30,7 @@ describe( "jRna", () => {
 
     it( "spawns instances with elements", (done) => {
         var rna = new jRna().element("myid");
-        var enzyme = rna.attach(root);
+        var enzyme = rna.attach(html('<div id="myid">some text</div>'));
 
         enzyme.should.have.property("myid");
         enzyme.myid.should.be.an.instanceof(jQuery);
