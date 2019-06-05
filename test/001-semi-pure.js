d0bffec6f6881f22525512545766bbb139e7042e
Konstantin S. Uvarin <khedin@gmail.com>
2019-06-05 03:31:25 +0300

    API Add jRna.dumbSwitch to define atomic sticky states

---
+++ test/001-semi-pure.js
@@ -0,0 +1,59 @@
+"use strict";
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+
+// never used, except for creating in-memory nodes
+const html = require( '../lib/test-mock-html.js' );
+
+const jRna = require( '../lib/jRna.js' );
+
+describe('jRna(no html)', () => {
+    it ('provides dumbSwitch functionality', done => {
+        const trace = [];
+
+        const rna = new jRna()
+            .html('<span></span>')
+            .def('flag', false)
+            .dumbSwitch( 'toggle', {
+                0 : function(olds, news) {
+                    this.flag = false;
+                    trace.push([0, olds, news]);
+                },
+                1 : function(olds, news) {
+                    this.flag = true;
+                    trace.push([1, olds, news]);
+                }
+            });
+
+        const inst = rna.spawn();
+
+        // initial state
+        trace.should.deep.equal([]);
+        should.not.exist( inst.toggle() );
+
+        // now switch it
+        inst.toggle(0);
+        inst.toggle().should.equal(0);
+        inst.flag.should.equal(false);
+        trace.should.deep.equal([[0, undefined, 0]]);
+
+        // try again = should stick
+        inst.toggle(0);
+        inst.toggle().should.equal(0);
+        inst.flag.should.equal(false);
+        trace.should.deep.equal([[0, undefined, 0]]);
+
+        // state change
+        inst.toggle(1);
+        inst.toggle().should.equal(1);
+        inst.flag.should.equal(true);
+        trace.should.deep.equal([[0, undefined, 0], [1, 0, 1]]);
+
+        const other = rna.spawn();
+        should.not.exist( other.toggle() );
+
+        done();
+    });
+});
+
