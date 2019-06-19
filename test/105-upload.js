212f96f375611b7529ed974d9585d02a9d385287
Konstantin S. Uvarin <khedin@gmail.com>
2019-06-20 02:52:15 +0300

    TST Add test for file uploads

--- test/105-upload.js
+++ test/105-upload.js
@@ -11,7 +11,7 @@ const jRna = require( '../lib/jrna.js' );
 
 const { File, FileList } = window;
 
-describe( 'jRna.upload', () => {
+describe( 'new jRna().upload', () => {
     const rna = new jRna()
         .html('<input type="file" class="jrna-upd">')
         .upload('upd');
@@ -21,8 +21,17 @@ describe( 'jRna.upload', () => {
         const probe = inst.container[0];
         expect( probe ).to.have.property( 'files' );
 
-        // TODO actually upload smth
-        done()
+        html.addFileList( probe, [ fakeFile('somedata') ] );
+        probe.dispatchEvent(new window.Event('input'));
+
+        const prom = inst.upd();
+        expect( prom ).to.be.instanceof(Promise);
+
+        prom.then(data => {
+            expect( data ).to.have.property( 'content', 'somedata' );
+
+            done();
+        });
     });
 
     it( 'creates promises via uploadFile', done => {
@@ -31,7 +40,6 @@ describe( 'jRna.upload', () => {
             'file.txt',
             { type: 'text/plain' }
         );
-
 
         const prom = jRna.uploadFile(file);
 
@@ -43,4 +51,41 @@ describe( 'jRna.upload', () => {
         });
     });
 });
+
+describe( 'jRna.upload() [static]', () => {
+    it( 'returns an upload promise', done => {
+        const root = html();
+
+        expect( root.find('input') ).to.have.property('length', 0);
+
+        const prom = jRna.upload();
+        expect( prom ).to.be.instanceof(Promise);
+
+        expect( root.find('input') ).to.have.property('length', 1);
+        const probe = root.find('input')[0];
+
+        // now the "user" uploads a "file"
+        html.addFileList( probe, [ fakeFile('food\nbard\nbazooka\n') ] );
+        probe.dispatchEvent(new window.Event('input'));
+
+        prom.then( data => {
+            // got the file back
+            expect( data ).to.have.property( 'content', 'food\nbard\nbazooka\n' );
+            expect( data ).to.have.property( 'size', 'food\nbard\nbazooka\n'.length );
+
+            // fake element removed from window
+            expect( root.find('input') ).to.have.property('length', 0);
+
+            done();
+        });
+    });
+});
+
+function fakeFile(content='', opts={}) {
+    return new File(
+        [ content ],
+        opts.name || 'file.txt',
+        { ...opts }
+    );
+};
 
